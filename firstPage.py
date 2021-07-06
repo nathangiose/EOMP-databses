beeb99559e29a094a27e98df7f4bf8b90febb2f8
nathangiose <nathangiose@gmail.com>
2021-07-06 09:34:14 +0200

    first commit

--- firstPage.py
+++ firstPage.py
@@ -10,74 +10,90 @@ from tkinter import messagebox
 import pymysql
 
 
-# Window Design
-win = Tk()
-win.title("LifeChoices Login")
-win.resizable(False, False)
-win.geometry("500x500")
-win.config(bg="#202020")
+# WINDOW DESIGN
 
 
-# Heading Label
-heading = Label(win, text="Login", font='Arial 25 bold', bg="#202020", fg="Green")
-heading.pack()
+window = Tk()
+window.title("LifeChoices Login")
+window.resizable(False, False)
+window.geometry("500x500")
+window.config(bg="black")
 
-username = Label(win, text="User Name :", font='Arial 15 bold', bg="#202020", fg="Green")
-username.place(x=80, y=60)
+# IMPORTING IMAGE
 
-userpass = Label(win, text="Password :", font='Arial 15 bold', bg="#202020", fg="Green")
-userpass.place(x=80, y=120)
+
+img = PhotoImage(file="output-onlinejpgtools.png")
+canvas = Canvas(window, width=262, height=116)
+canvas.create_image(0, 0, anchor=NW, image=img)
+canvas.place(x=126, y=300)
+
+
+# LABELS
+
+
+header = Label(window, text="Login", font='Arial 25 bold', bg="black", fg="Green")
+header.pack()
+
+username = Label(window, text="Username :", font='Arial 15 bold', bg="black", fg="Green")
+username.place(x=60, y=60)
+
+password = Label(window, text="Password :", font='Arial 15 bold', bg="black", fg="Green")
+password.place(x=60, y=120)
 
 # Entry Box
+
+
 user_name = StringVar()
 password = StringVar()
 
-userentry = Entry(win, width=35, textvariable=user_name)
+userentry = Entry(window, width=35, textvariable=user_name)
 userentry.focus()
-userentry.place(x=200, y=60)
+userentry.place(x=190, y=60)
 
-passentry = Entry(win, width=35, show="*", textvariable=password)
-passentry.place(x=200, y=120)
+passentry = Entry(window, width=35, show="*", textvariable=password)
+passentry.place(x=190, y=120)
 
 
 def clear():
 	userentry.delete(0, END)
 	passentry.delete(0, END)
 
+
 def close():
-	win.destroy()
+	window.destroy()
 
 
 def login():
 	if user_name.get() == "" or password.get() == "":
-		messagebox.showerror("Error", "Enter User Name And Password", parent=win)
+		messagebox.showerror("Error", "Enter User Name And Password", parent=window)
 	else:
 		try:
 			con = pymysql.connect(host="localhost", user="root", password="", database="docterapp")
 			cur = con.cursor()
 
 			cur.execute("select * from user_information where username=%s and password = %s", (user_name.get(), password.get()))
-			row = cur.fetchone()
+			info = cur.fetchone()
 
-			if row==None:
-				messagebox.showerror("Error", "Invalid User Name And Password", parent=win)
+			if info == None:
+				messagebox.showerror("Error", "Invalid User Name And Password", parent=window)
 
 			else:
-				messagebox.showinfo("Success", "Successfully Login", parent=win)
+				messagebox.showinfo("Success", "Successfully Login", parent=window)
 				close()
-				#dashboard()
+				# import new window here
 			con.close()
 		except Exception as es:
-			messagebox.showerror("Error", f"Error Dui to : {str(es)}", parent=win)
+			messagebox.showerror("Error", f"Error Due To : {str(es)}", parent=window)
 
 
 # button login and clear
 
-btn_login = Button(win, text="Login", font='Arial 12 bold', command=login)
-btn_login.place(x=200, y=293)
+
+btn_login = Button(window, text="Login", bd=4, font='Arial 12 bold', command=login, bg="Black", fg="Green")
+btn_login.place(x=190, y=180)
 
 
-btn_login = Button(win, text="Clear", font='Arial 12 bold', command=clear)
-btn_login.place(x=260, y=293)
+btn_login = Button(window, text="Clear", bd=4, font='Arial 12 bold', command=clear, bg="Green", fg="White")
+btn_login.place(x=270, y=180)
 
-win.mainloop()
+window.mainloop()
